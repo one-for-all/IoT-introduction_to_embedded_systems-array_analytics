0ed4e6e164d6e764da9302919f301c3bf75a17cd
one-for-all <jianan.li@yahoo.com>
2017-09-18 17:44:19 -0700

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -2,20 +2,21 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Print the statistics of the array
  *
- * <Add Extended Description Here>
+ * Analyze the array and print its statistics including maximum, minimum, mean,
+ * and median
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Lance Li
+ * @date 09/18/17
  *
  */
 
@@ -27,7 +28,7 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -40,4 +41,16 @@ void main() {
 
 }
 
-/* Add other Implementation File Code Here */
+void print_statistics(unsigned char *array, unsigned int size) {}
+
+void print_array(unsigned char *array, unsigned int size) {}
+
+unsigned char find_median(unsigned char *array, unsigned int size) {}
+
+unsigned char find_mean(unsigned char *array, unsigned int size) {}
+
+unsigned char find_maximum(unsigned char *array, unsigned int size) {}
+
+unsigned char find_minimum(unsigned char *array, unsigned int size) {}
+
+void sort_array(unsigned char *array, unsigned int size) {}
--- stats.h
+++ stats.h
@@ -2,39 +2,107 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header file for stats.c
  *
- * <Add Extended Description Here>
+ * Header file for a program that analyzes an array of numbers
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Lance Li
+ * @date 09/18/17
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief print statistics of the array
+ *
+ * Print statistics including minimum, maximum, mean, and median.
+ *
+ * @param array An array of unsigned chars
+ * @param size The number of elements in the array
+ *
+ * @return void
+ */
+void print_statistics(unsigned char *array, unsigned int size);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief print the array
  *
- * <Add Extended Description Here>
+ * Print the array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param array An array of unsigned chars
+ * @param size The number of elements in the array
  *
- * @return <Add Return Informaiton here>
+ * @return void
  */
+void print_array(unsigned char *array, unsigned int size);
 
+/**
+ * @brief returns the median of the array
+ *
+ * Compute and return the median of the array
+ *
+ * @param array An array of unsigned chars
+ * @param size The number of elements in the array
+ *
+ * @return The median
+ */
+unsigned char find_median(unsigned char *array, unsigned int size);
+
+/**
+ * @brief returns the mean of the array
+ *
+ * Compute and return the mean of the array
+ *
+ * @param array An array of unsigned chars
+ * @param size The number of elements in the array
+ *
+ * @return The mean
+ */
+unsigned char find_mean(unsigned char *array, unsigned int size);
+
+/**
+ * @brief returns the maximum of the array
+ *
+ * Compute and return the maximum of the array
+ *
+ * @param array An array of unsigned chars
+ * @param size The number of elements in the array
+ *
+ * @return The maximum
+ */
+unsigned char find_maximum(unsigned char *array, unsigned int size);
+
+/**
+ * @brief returns the minimum of the array
+ *
+ * Compute and return the minimum of the array
+ *
+ * @param array An array of unsigned chars
+ * @param size The number of elements in the array
+ *
+ * @return The minimum
+ */
+unsigned char find_minimum(unsigned char *array, unsigned int size);
+
+/**
+ * @brief sort the array in descending order
+ *
+ * Sort the array from largest to smallest in-place
+ *
+ * @param array An array of unsigned chars
+ * @param size The number of elements in the array
+ *
+ * @return void
+ */
+void sort_array(unsigned char *array, unsigned int size);
 
 #endif /* __STATS_H__ */
